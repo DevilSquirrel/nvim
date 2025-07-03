ebc43131d41c589c2a9f5804ad4c74b38beb220d
liuyuchuan <123456789>
2025-07-03 18:52:34 +0800

    windows file

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- local map = LazyVim.safe_keymap_set
+local keymap = vim.keymap
+--插入模式
+keymap.set("i", "jk", "<esc>")
+keymap.set("i", "<esc>", "<nop>")
